c27d19d37cca5528a9ebd935fe9a7608c5314e22
Kimminsu <alstn2468_@naver.com>
2020-04-28 10:47:09 +0900

    Updates

---
+++ precache-manifest.3d5a1cb6da48bf95e852865a181d7223.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac949e4c38b153aa8ed9b303717bfa7a",
+    "url": "/Redux_Kanban_Web/index.html"
+  },
+  {
+    "revision": "58907c14098dc6b86842",
+    "url": "/Redux_Kanban_Web/static/js/2.f318f619.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/Redux_Kanban_Web/static/js/2.f318f619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3fb09193ec323383e95",
+    "url": "/Redux_Kanban_Web/static/js/main.00cc6571.chunk.js"
+  },
+  {
+    "revision": "efeebdc2f9ffdccc6bc1",
+    "url": "/Redux_Kanban_Web/static/js/runtime-main.aba4305d.js"
+  },
+  {
+    "revision": "89799566ae64380797abc3f6fc450346",
+    "url": "/Redux_Kanban_Web/static/media/moon.89799566.svg"
+  },
+  {
+    "revision": "fbbe405ec9ef5e96f231c3c6ae8a7b81",
+    "url": "/Redux_Kanban_Web/static/media/sun.fbbe405e.svg"
+  }
+]);
